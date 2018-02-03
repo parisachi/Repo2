e35159c839b78082980f5109aac783854f0998ba
parisachi <parisachi@email.com>
2018-02-03 20:18:13 +0200

    Hello

---
+++ HelloWorld.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+
+function printstuff(){
+    console.log("stuff");
+}
+
+
+console.log(__filename);
+
+console.log(__dirname);
+
+setTimeout(printstuff,5000);
+
+setInterval(printstuff,1000);
